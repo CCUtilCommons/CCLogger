79ae7053f463332c8b39efb5dbffd2d116471114
Charliechen114514 <725610365@qq.com>
2025-11-18 00:20:08 +0800

    fix export macro

--- logger/CCLoggerExports.h
+++ logger/CCLoggerExports.h
@@ -1,19 +1,19 @@
 #pragma once
 
-// Detect Windows
-#if defined(_WIN32) || defined(_WIN64)
-#define CCLOG_PLATFORM_WINDOWS
-#endif
-
-#if defined(CCLOG_PLATFORM_WINDOWS)
-#if defined(CCLOG_BUILD_SHARED)
+#if defined(_WIN32) || defined(__CYGWIN__)
+#ifdef CCLOG_BUILD_SHARED
+// Building CCLogger as a DLL
 #define CCLOG_API __declspec(dllexport)
-#else
+#elif defined(CCLOG_USE_SHARED)
+// Using CCLogger DLL
 #define CCLOG_API __declspec(dllimport)
+#else
+// Static library or no DLL
+#define CCLOG_API
 #endif
 #else
-// GCC/Clang: use visibility attributes
-#if defined(CCLOG_BUILD_SHARED)
+// Linux/macOS
+#ifdef CCLOG_BUILD_SHARED
 #define CCLOG_API __attribute__((visibility("default")))
 #else
 #define CCLOG_API
